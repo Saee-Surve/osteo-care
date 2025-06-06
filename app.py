d85666338c1e547ed35821b009f424b44263179d
Saee Surve <161334162+Saee-Surve@users.noreply.github.com>
2025-06-06 15:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,4 +136,4 @@ def prediction():
     return render_template('prediction.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
